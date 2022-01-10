2c6f6778cd32494ce774d9cc620d9b50b80ce34e
노지원 <rojiwon@MacAir.local>
2022-01-10 21:18:35 +0900

    project setup

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { RestaurantModule } from './restaurant/restaurant.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    RestaurantModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
